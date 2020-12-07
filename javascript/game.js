0769c142ec1b35c8429328294c5782f2ca1a17b9
erkartali <erkartali@gmail.com>
2020-12-07 17:34:15 -0600

    created variables and random function

--- javascript/game.js
+++ javascript/game.js
@@ -1,1 +1,19 @@
-console.log('balls')
+let wins = 0;
+const words = {
+    thing1: 'one',
+    thing2: 'two',
+    thing3: 'three',
+    thing4: 'four',
+    thing5: 'five',
+};
+const guessesRemaining = 12;
+const lettersGuessed = []
+const alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
+
+//shuffle function
+const randomProperty = (obj) => {
+    var keys = Object.keys(obj);
+    return obj[keys[ keys.length * Math.random() << 0]];
+};
+
+console.log(randomProperty(words));
