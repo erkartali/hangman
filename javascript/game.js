099f3ecc735be26c2b420f015fbe928e395bc844
erkartali <erkartali@gmail.com>
2020-12-08 11:02:33 -0600

    created loop to output dashes

--- javascript/game.js
+++ javascript/game.js
@@ -1,19 +1,68 @@
 let wins = 0;
-const words = {
-    thing1: 'one',
-    thing2: 'two',
-    thing3: 'three',
-    thing4: 'four',
-    thing5: 'five',
-};
-const guessesRemaining = 12;
-const lettersGuessed = []
+const words = ['one','two','three','four','five'];
+let guessesRemaining = 12;
+let lettersGuessed = [];
+let lettersOfWord = [];
+let dashes = [];
 const alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
 
 //shuffle function
-const randomProperty = (obj) => {
-    var keys = Object.keys(obj);
-    return obj[keys[ keys.length * Math.random() << 0]];
-};
+randomWord = words[Math.floor(Math.random() * words.length)];
 
-console.log(randomProperty(words));
+// splits the word selected into individual letters
+lettersOfWord = randomWord.split('');
+
+// loop through the letters of the word and add dashes 
+for (let i = 0; i < lettersOfWord.length; i++) {
+    dashes.push('_');
+}
+
+    console.log(`dashes: ${dashes}`);
+
+// get the key pressed by the user
+document.onkeyup = (e) => {
+    const keyPressed = e.key;
+
+    if(alphabet.includes(keyPressed)) {
+
+        // if key pressed IS NOT in lettersGuessed array
+        if (!lettersGuessed.includes(keyPressed)) {
+
+            // if letter guessed is in word
+            if (keyPressed) {}
+
+                // add to letters guessed array
+
+                // replace _ with letter
+
+                // if word is filled in completely
+
+                    // run win function
+
+            // if letter guessed is not in word
+
+                // add letter to lettersGuessed array
+
+                // reduce number of guesses remaining by 1
+
+                // if guesses remaining is 0
+                
+                    // run you lose function 
+        }
+
+        console.log('balls');
+
+    }
+}
+
+// output guesses remaining to html
+document.getElementById('guesses-remaining').innerHTML = `<span>${guessesRemaining}</span>`;
+
+// output wins to html
+document.getElementById('wins').innerHTML = `<span>${wins}</span>`;
+
+// output letters guessed to html
+lettersGuessed.forEach(element => {
+    document.getElementById("letters-guessed").innerHTML= JSON.stringify(lettersGuessed); 
+ });
+
